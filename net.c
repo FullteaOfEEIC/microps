810d85412246c0e7e6980482100cca16c4a97ab2
Rikuta Furuta <rikuta@furutan.com>
2022-03-18 23:14:01 +0900

    step22

--- net.c
+++ net.c
@@ -12,6 +12,7 @@
 #include "ip.h"
 #include "icmp.h"
 #include "udp.h"
+#include "tcp.h"
 
 struct net_protocol {
     struct net_protocol *next;
@@ -339,6 +340,10 @@ int net_init(void){
     }
     if (udp_init() == -1) {
         errorf("udp_init() failure");
+        return -1;
+    }
+    if (tcp_init() == -1) {
+        errorf("tcp_init() failure");
         return -1;
     }
     infof("initialized");
